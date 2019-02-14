2ae8bed790d7ff8736ed6086fd1ada5bd270a755
jaredasch1 <jaredasch1@gmail.com>
2019-02-13 22:20:28 -0500

    added image to draw, testing

--- draw.c
+++ draw.c
@@ -10,8 +10,8 @@
 Inputs:   struct matrix * points
          int x
          int y
-         int z 
-Returns: 
+         int z
+Returns:
 adds point (x, y, z) to points and increment points.lastcol
 if points is full, should call grow on points
 ====================*/
@@ -30,12 +30,12 @@ void add_point( struct matrix * points, double x, double y, double z) {
 /*======== void add_edge() ==========
 Inputs:   struct matrix * points
           int x0, int y0, int z0, int x1, int y1, int z1
-Returns: 
+Returns:
 add the line connecting (x0, y0, z0) to (x1, y1, z1) to points
 should use add_point
 ====================*/
-void add_edge( struct matrix * points, 
-	       double x0, double y0, double z0, 
+void add_edge( struct matrix * points,
+	       double x0, double y0, double z0,
 	       double x1, double y1, double z1) {
   add_point(points, x0, y0, z0);
   add_point(points, x1, y1, z1);
@@ -44,8 +44,8 @@ void add_edge( struct matrix * points,
 /*======== void draw_lines() ==========
 Inputs:   struct matrix * points
          screen s
-         color c 
-Returns: 
+         color c
+Returns:
 Go through points 2 at a time and call draw_line to add that line
 to the screen
 ====================*/
@@ -55,95 +55,86 @@ void draw_lines( struct matrix * points, screen s, color c) {
   }
 }
 
+void draw_line_OCT1(int x0, int y0, int x1, int y1, screen s, color c){
+    int a = y1 - y0;
+    int b = x0 - x1;
+    int d = 2 * a + b;
+    int x = x0; int y = y0;
+    while(x <= x1){
+        plot(s, c, x, y);
+        if(d > 0){
+            y++;
+            d += 2 * b;
+        }
+        x++;
+        d += 2 * a;
+    }
+}
+
+void draw_line_OCT2(int x0, int y0, int x1, int y1, screen s, color c){
+    int a = y1 - y0;
+    int b = x0 - x1;
+    int d = a + 2 * b;
+    int x = x0; int y = y0;
+    while(y <= y1){
+        plot(s, c, x, y);
+        if(d < 0){
+            x++;
+            d += 2 * a;
+        }
+        y++;
+        d += 2 * b;
+    }
+}
+
+void draw_line_OCT7(int x0, int y0, int x1, int y1, screen s, color c){
+    int a = y1 - y0;
+    int b = x0 - x1;
+    int d = a - 2 * b;
+    int x = x0; int y = y0;
+    while(y >= y1){
+        plot(s, c, x, y);
+        if(d > 0){
+            x++;
+            d += 2 * a;
+        }
+        y--;
+        d -= 2 * b;
+    }
+}
+
+void draw_line_OCT8(int x0, int y0, int x1, int y1, screen s, color c){
+    int a = y1 - y0;
+    int b = x0 - x1;
+    int d = 2 * a - b;
+    int x = x0; int y = y0;
+    while(x <= x1){
+        plot(s, c, x, y);
+        if(d < 0){
+            y--;
+            d -= 2 * b;
+        }
+        x++;
+        d += 2 * a;
+    }
+}
+
 void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
-
-  int x, y, d, A, B;
-  //swap points if going right -> left
-  int xt, yt;
-  if (x0 > x1) {
-    xt = x0;
-    yt = y0;
-    x0 = x1;
-    y0 = y1;
-    x1 = xt;
-    y1 = yt;
-  }
-  
-  x = x0;
-  y = y0;
-  A = 2 * (y1 - y0);
-  B = -2 * (x1 - x0);  
-
-  //octants 1 and 8
-  if ( abs(x1 - x0) >= abs(y1 - y0) ) {
-
-    //octant 1    
-    if ( A > 0 ) {
-      
-      d = A + B/2;      
-      while ( x < x1 ) {
-	plot( s, c, x, y );
-	if ( d > 0 ) {
-	  y+= 1;
-	  d+= B;
-	}
-	x++;
-	d+= A;
-      } //end octant 1 while
-      plot( s, c, x1, y1 );
-    } //end octant 1
-
-    //octant 8
-    else {
-      d = A - B/2;
-      
-      while ( x < x1 ) {
-	//printf("(%d, %d)\n", x, y);
-	plot( s, c, x, y );
-	if ( d < 0 ) {
-	  y-= 1;
-	  d-= B;
-	}
-	x++;
-	d+= A;
-      } //end octant 8 while
-      plot( s, c, x1, y1 );
-    } //end octant 8
-  }//end octants 1 and 8
-
-  //octants 2 and 7
-  else {
-    
-    //octant 2    
-    if ( A > 0 ) {
-      d = A/2 + B;      
-
-      while ( y < y1 ) {
-	plot( s, c, x, y );
-	if ( d < 0 ) {
-	  x+= 1;
-	  d+= A;
-	}
-	y++;
-	d+= B;
-      } //end octant 2 while
-      plot( s, c, x1, y1 );
-    } //end octant 2
-
-    //octant 7
-    else {
-      d = A/2 - B;
-      
-      while ( y > y1 ) {
-	plot( s, c, x, y );
-	if ( d > 0 ) {
-	  x+= 1;
-	  d+= A;
-	}
-	y--;
-	d-= B;
-      } //end octant 7 while
-      plot( s, c, x1, y1 );
-    } //end octant 7   
-  }//end octants 2 and 7  
-} //end draw_line
+    if(x1 < x0){
+        draw_line(x1, y1, x0, y0, s, c);
+    } else {
+        if(y1 - y0 < 0){
+            if(y0 - y1 >= x1 - x0){
+                draw_line_OCT7(x0, y0, x1, y1, s, c);
+            } else {
+                draw_line_OCT8(x0, y0, x1, y1, s, c);
+            }
+        } else {
+            if(y1 - y0 <= x1 - x0){
+                draw_line_OCT1(x0, y0, x1, y1, s, c);
+            } else {
+                draw_line_OCT2(x0, y0, x1, y1, s, c);
+            }
+        }
+    }
+}
--- main.c
+++ main.c
@@ -11,20 +11,45 @@ int main() {
 
   screen s;
   color c;
+
   c.red = 255;
-  struct matrix *edges;
+  clear_screen(s);
 
+  struct matrix * edges = new_matrix(4, 4);
 
-  
-  edges = new_matrix(4, 4);
-  add_edge(edges, 100, 100, 0, 250, 250, 0);
-  add_edge(edges, 250, 250, 0, 400, 100, 0);
+  add_edge(edges, 100, 100, 0, 250, 300, 0);
+  add_edge(edges, 250, 300, 0, 400, 100, 0);
   add_edge(edges, 400, 100, 0, 100, 100, 0);
+  add_edge(edges, 175, 200, 0, 250, 225, 0);
+  add_edge(edges, 325, 200, 0, 250, 225, 0);
 
-  draw_lines(edges, s, c);
-  
-  save_extension(s, "lines.png");
+  add_edge(edges, 175, 200, 0, 250, 175, 0);
+  add_edge(edges, 325, 200, 0, 250, 175, 0);
 
+  add_edge(edges, 250, 175, 0, 225, 200, 0);
+  add_edge(edges, 250, 225, 0, 225, 200, 0);
+  add_edge(edges, 250, 175, 0, 275, 200, 0);
+  add_edge(edges, 250, 225, 0, 275, 200, 0);
+
+
+  struct matrix * iden = new_matrix(4, 4);
+  ident(iden);
+  printf("Printing 4 x 4 identity matrix...\n");
+  print_matrix(iden);
+
+  printf("Printing edge matrix...\n");
   print_matrix(edges);
+
+  printf("Multiplying identity matrix by edge matrix...(should have no effect)\n");
+  matrix_mult(iden, edges); // Should have no effect
+
+  printf("Printing new edge matrix...\n");
+  print_matrix(edges);
+  draw_lines(edges, s, c);
+
+  printf("Drawing lines from edge matrix...\n");
+  save_extension(s, "lines.png");
+  printf("Image saved to lines.png\n");
+
   free_matrix( edges );
-}  
+}
