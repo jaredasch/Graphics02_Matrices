0b974f87b32922c882c8c036c6bf77265348c20c
jaredasch1 <jaredasch1@gmail.com>
2019-02-24 21:51:07 -0500

    added more testing

--- main.c
+++ main.c
@@ -15,6 +15,22 @@ int main() {
   c.red = 255;
   clear_screen(s);
 
+  struct matrix * m1 = new_matrix(4, 4);
+  add_edge(m1, 1, 2, 3, 4, 5, 6);
+  add_edge(m1, 7, 8, 9, 10, 11, 12);
+  printf("Printing 4 x 4 matrix m1\n");
+  print_matrix(m1);
+
+  struct matrix * m2 = new_matrix(4, 4);
+  add_edge(m2, -1, 0, 1, 3, 2, 1);
+  add_edge(m2, 1, 2, -1, 0, 1, 2);
+  printf("Printing 4 x 4 matrix m2\n");
+  print_matrix(m2);
+
+  printf("Printing m1 x m2\n");
+  matrix_mult(m1, m2);
+  print_matrix(m2);
+
   struct matrix * edges = new_matrix(4, 4);
 
   add_edge(edges, 100, 100, 0, 250, 300, 0);
